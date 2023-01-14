89915718192a1d629f745efa797b8575b7367260
RubenChet <rubenchetboun@gmail.com>
2023-01-14 22:25:25 +0100

    tondaron

--- Vue/src/components/Header.vue
+++ Vue/src/components/Header.vue
@@ -1,8 +1,11 @@
 <template>
-    <div class="flex justify-center mt-10">
-        <div class="w-1/2 flex justify-between">
-            <p>Logo</p>
-            <p>Account</p>
-        </div>
+  <div class="flex justify-center mt-10">
+    <div class="w-1/2 flex justify-between">
+      <div class="flex items-center space-x-2">
+        <img src="../assets/logo.png" alt="" class="w-12" />
+        <h1>TypeZone</h1>
+      </div>
+      <p>Account</p>
     </div>
+  </div>
 </template>
